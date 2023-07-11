88c91f1f53fdbf963044db8837c1349d13183dab
jinyoung <sm099@naver.com>
2023-07-12 00:28:28 +0900

    feat : 만약 유저가 입력한 숫자 중 0이 포함된다면 throw 문을 통해 예외를 발생시킨 후 게임을 종료 시킨다.

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -7,6 +7,9 @@ const validatedValue = (inputValue) => {
   if (isTypeOfNaN(inputValue)) {
     throw new Error("숫자만 입력이 가능합니다. 숫자를 3자리로 입력해주세요.");
   }
+  if (String(inputValue).includes("0")) {
+    throw new Error("1부터 9까지의 숫자만 입력해주세요.");
+  }
 };
 
 module.exports = validatedValue;
